1ad956a722fa5c6487a59dd59714179b5b20338e
Didier Peran Ganthier <didierganthierperan@gmail.com>
2025-01-24 14:07:30 -0500

    add CounterWidget with basic structure

---
+++ lib/widgets/counter_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CounterWidget extends StatelessWidget {
+  const CounterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
